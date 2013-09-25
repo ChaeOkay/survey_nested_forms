537901a9b05ab89a305dd121ab58bd31adeff1e6
cokeefe <chaemobile@gmail.com>
2013-09-25 10:30:36 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SurveySays::Application.config.secret_token = '9d70a71cabbc4adf628d8bf237efab9f983ddaa12cf68a08b678383bf7b8bff5cd7f914717af2701e3b6e4311ed16e7581772be81209d02e034b2e287d05f9ec'
---
+++ spec/spec_helper.rb
@@ -0,0 +1,39 @@
+# This file is copied to spec/ when you run 'rails generate rspec:install'
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
+require 'rspec/autorun'
+
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+
+RSpec.configure do |config|
+
+  config.include Capybara::DSL
+  config.mock_with :rspec
+  config.include AuthenticationHelper
+  config.include FactoryGirl::Syntax::Methods
+
+  config.use_transactional_fixtures = false
+  # true rolls back but doesn't work with capybara
+  # capybara runs on diff thread
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :truncation
+    # later on maybe
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
+
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.infer_base_class_for_anonymous_controllers = false
+  config.order = "random"
+end
