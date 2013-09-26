0b0e44a39e2b79329fcd0bac09b5ccf5ab6bf7d2
cokeefe <chaemobile@gmail.com>
2013-09-25 17:04:24 -0700

    create survey question form template

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,9 +1,18 @@
 class SurveysController < ApplicationController
-  def create
-    @survey = Survey.create(params[:survey])
-  end
 
   def new
     @survey = Survey.new
+    @survey.questions.build
   end
+
+  def create
+    @survey = Survey.create(params[:survey])
+
+    if @survey.save
+      redirect_to new_survey_path, :notice => "Survey successfully saved"
+    else
+      redirect_to new_survey_path, :notice => @survey.errors.full_messages.join(', ')
+    end
+  end
+
 end
--- spec/features/survey_features_spec.rb
+++ spec/features/survey_features_spec.rb
@@ -1,14 +1,14 @@
 require 'spec_helper'
 
-feature 'Survey' do
+feature 'Survey', :js => true do
   describe 'making a survey' do
     context 'valid params' do
       it 'should display a success message' do
         visit new_survey_path
         fill_in 'survey_title', with: "Yo Mama's Survey"
-        # fill_in 'survey_question_description', with: "How phat?"
-        # click_button "Create Amazing Survey"
-        # expect(page.body).to have_content("Survey successfully saved")
+        fill_in 'survey_questions_attributes_0_description', with: "How phat?"
+        click_button "Create Amazing Survey"
+        expect(page.body).to have_content("Survey successfully saved")
       end
     end
   end
