47a82ae91d61e13d40b8dee5804574f3f3e5608c
cokeefe <chaemobile@gmail.com>
2013-09-25 18:03:31 -0700

    form partial for questions

--- spec/features/survey_features_spec.rb
+++ spec/features/survey_features_spec.rb
@@ -5,6 +5,7 @@ feature 'Survey', :js => true do
     context 'valid params' do
       it 'should display a success message' do
         visit new_survey_path
+        click_button "Add Question"
         fill_in 'survey_title', with: "Yo Mama's Survey"
         fill_in 'survey_questions_attributes_0_description', with: "How phat?"
         click_button "Create Amazing Survey"
